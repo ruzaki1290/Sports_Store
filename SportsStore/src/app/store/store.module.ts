a00d1d3cc33ff300241bdf108ee55243699755b9
Rus <rus.zakirov@triosstudent.com>
2024-10-28 23:19:46 -0400

    Added a cart

--- SportsStore/src/app/store/store.module.ts
+++ SportsStore/src/app/store/store.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
 import { CounterDirective } from "./counter.directive";
+import { CartSummaryComponent } from "./cartSummary.component";
 @NgModule({
  imports: [ModelModule, CommonModule, FormsModule],
- declarations: [StoreComponent, CounterDirective],
+ declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
  exports: [StoreComponent]
 })
 export class StoreModule { }
