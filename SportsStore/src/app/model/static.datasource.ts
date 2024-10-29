5c622ed3f8f589897b5de0985377659250d29845
Rus <rus.zakirov@triosstudent.com>
2024-10-28 21:24:12 -0400

    Modified multiple files

--- SportsStore/src/app/model/static.datasource.ts
+++ SportsStore/src/app/model/static.datasource.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Product } from "./product.model";
-import { Observable, of } from "rxjs";
+import { Observable, from, of } from "rxjs";
 @Injectable({ providedIn: 'root' })
 export class StaticDataSource {
  private products: Product[] = [
