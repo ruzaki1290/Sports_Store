0cd4a263b5641cc179f427da6f5e33b2861e2e0b
Rus <rus.zakirov@triosstudent.com>
2024-10-28 20:47:22 -0400

    Fixed some bugs in multiple files

--- SportsStore/src/app/app.component.ts
+++ SportsStore/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { StoreModule } from './store/store.module';
 
 @Component({
   selector: 'app',
+  // standalone: true,
+  // imports: [StoreModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
